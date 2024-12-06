99569e6229acc409618e7ba231c3734221e16c7c
Fernando Rodrigues <85629970+Fernando7181@users.noreply.github.com>
2024-12-06 08:44:13 -0300

    Added user profile picture
    Have added a user profile picture on the top right and removed the run sync name

--- run_sync/lib/screens/home_screen.dart
+++ run_sync/lib/screens/home_screen.dart
@@ -6,12 +6,31 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final String userProfileUrl = 'https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png'; // Replace with actual URL
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Run Sync'),
+        title: Row(
+          children: [
+            const SizedBox(width: 10),
+            CircleAvatar(
+              backgroundImage: NetworkImage(userProfileUrl),
+            ),
+          ],
+        ),
         actions: [
           Builder(
             builder: (context) {
+                return Row(
+                children: [
+                  IconButton(
+                  icon: const Icon(Icons.menu), // Hamburger menu icon
+                  onPressed: () {
+                    Scaffold.of(context).openEndDrawer(); // Open drawer from the right
+                  },
+                  ),
+                  const SizedBox(width: 10),
+                ],
+                );
               return IconButton(
                 icon: const Icon(Icons.menu), // Hamburger menu icon
                 onPressed: () {
