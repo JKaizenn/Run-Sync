8c22943f1c32a47fd37014f95e50721aa70ad703
n00nchi <alfredooviedo@Alfredos-MBP.lan>
2024-12-10 22:33:09 -0600

    add and upload pages

--- run_sync/lib/screens/home_screen.dart
+++ run_sync/lib/screens/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:run_sync/screens/upload_page.dart';
 import '../widgets/app_navigation_drawer.dart';
 import '../widgets/workout_log.dart';
+import 'add_page.dart';  
+import 'upload_page.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -53,6 +56,40 @@ class HomeScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: WorkoutLog(workoutsStream: getWorkoutsStream()),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Add',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.arrow_upward),
+            label: 'Upload',
+          ),
+        ],
+        currentIndex: 1, // Set the default selected tab index (e.g., Home is selected here)
+        onTap: (index) {
+          if (index == 0) {
+            // Navigate to AddPage
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const AddPage()),
+            );
+          } else if (index == 1) {
+            // Stay on Home screen (do nothing)
+          } else if (index == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const UploadPage()),   
+            );
+            // Add logic for Upload screen if needed
+          }
+        },
       ),
     );
   }
